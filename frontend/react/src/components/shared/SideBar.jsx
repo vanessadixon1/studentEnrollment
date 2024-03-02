26d99d3017be32821f56ab440411c7733ff9044c
vanessadixon1 <mclendonfamily88@gmail.com>
2024-03-02 10:14:15 -0500

    cors update

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -114,7 +114,7 @@ const NavItem = ({icon, children, ...rest}) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: 'cyan.400',
+                    bg: 'green.400',
                     color: 'white',
                 }}
                 {...rest}>

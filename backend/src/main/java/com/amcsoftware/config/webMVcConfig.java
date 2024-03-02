3c9efc494e28d1526f86ec7996a4f96f04f99a97
vanessadixon1 <mclendonfamily88@gmail.com>
2024-03-02 05:45:37 -0500

    cors changees

--- backend/src/main/java/com/amcsoftware/config/webMVcConfig.java
+++ backend/src/main/java/com/amcsoftware/config/webMVcConfig.java
@@ -1,9 +1,11 @@
 package com.amcsoftware.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,16 +13,31 @@ import java.util.List;
 @Configuration
 public class webMVcConfig implements WebMvcConfigurer {
 
-    @Value("#{'${cors.allowed-origins}'.split(',')}")
-    private List<String> allowedOrigins;
+    @Configuration
+    public class CorsConfig {
 
-    @Value("#{'${cors.allowed-methods}'.split(',')}")
-    private List<String> allowedMethods;
+        @Value("#{'${cors.allowed-origins}'.split(',')}")
+        private List<String> allowedOrigins;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        CorsRegistration corsRegistration = registry.addMapping("/api/**");
-        allowedOrigins.forEach(corsRegistration::allowedOrigins);
-        allowedMethods.forEach(corsRegistration::allowedMethods);
+        @Value("#{'${cors.allowed-methods}'.split(',')}")
+        private List<String> allowedMethods;
+
+        @Value("#{'${cors.allowed-headers}'.split(',')}")
+        private List<String> allowedHeaders;
+
+        @Value("#{'${cors.exposed-headers}'.split(',')}")
+        private List<String> expectedHeaders;
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(allowedOrigins);
+            configuration.setAllowedMethods(allowedMethods);
+            configuration.setAllowedHeaders(allowedHeaders);
+            configuration.setExposedHeaders(expectedHeaders);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/api/**", configuration);
+            return source;
+        }
     }
 }

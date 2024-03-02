6300824fe077a2500dd8814b4b9995e9db44e8f0
vanessadixon1 <mclendonfamily88@gmail.com>
2024-03-02 08:04:15 -0500

    cors update

--- backend/src/main/java/com/amcsoftware/PingPongController.java
+++ backend/src/main/java/com/amcsoftware/PingPongController.java
@@ -14,7 +14,7 @@ public class PingPongController {
 
     @GetMapping("/k")
     public PingPong rano() {
-        return new PingPong("new resta was sammie");
+        return new PingPong("new resta was sammiei");
     }
 
 
